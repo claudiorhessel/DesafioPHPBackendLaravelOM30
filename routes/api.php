d1cadb2055053f276cd9a217f448917ad07b4549
Claudio Hessel <claudiorhessel@gmail.com>
2023-03-28 06:11:20 -0300

    feat: create initial api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AddressApiController;
+use App\Http\Controllers\Api\AddressTypeApiController;
+use App\Http\Controllers\Api\FileApiController;
+use App\Http\Controllers\Api\FileTypeApiController;
+use App\Http\Controllers\Api\PatientApiController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +27,46 @@ Route::group(['prefix' => 'v1'], function() {
                 return $request->user();
             });
         });
+
+        Route::group(['prefix' => 'patient'], function() {
+            Route::get('/', [PatientApiController::class, 'index']);
+            Route::get('/{id}', [PatientApiController::class, 'show']);
+            Route::post('/', [PatientApiController::class, 'store']);
+            Route::put('/{id}', [PatientApiController::class, 'login']);
+            Route::delete('/{id}', [PatientApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'address_type'], function() {
+            Route::get('/', [AddressTypeApiController::class, 'index']);
+            Route::get('/{id}', [AddressTypeApiController::class, 'show']);
+            Route::post('/', [AddressTypeApiController::class, 'store']);
+            Route::put('/{id}', [AddressTypeApiController::class, 'update']);
+            Route::delete('/{id}', [AddressTypeApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'address'], function() {
+            Route::get('/', [AddressApiController::class, 'index']);
+            Route::get('/{id}', [AddressApiController::class, 'show']);
+            Route::post('/', [AddressApiController::class, 'store']);
+            Route::put('/{id}', [AddressApiController::class, 'update']);
+            Route::delete('/{id}', [AddressApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'file_type'], function() {
+            Route::get('/', [FileTypeApiController::class, 'index']);
+            Route::get('/{id}', [FileTypeApiController::class, 'show']);
+            Route::post('/', [FileTypeApiController::class, 'store']);
+            Route::put('/{id}', [FileTypeApiController::class, 'update']);
+            Route::delete('/{id}', [FileTypeApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'file'], function() {
+            Route::get('/', [FileApiController::class, 'index']);
+            Route::get('/{id}', [FileApiController::class, 'show']);
+            Route::post('/', [FileApiController::class, 'store']);
+            Route::put('/{id}', [FileApiController::class, 'update']);
+            Route::delete('/{id}', [FileApiController::class, 'destroy']);
+        });
     });
 
     Route::group(['prefix' => 'auth'], function() {
@@ -30,10 +75,10 @@ Route::group(['prefix' => 'v1'], function() {
     });
 });
 
-Route::any('{segment}', function () {
-    return response()->json(['error' => 'Bad request.'], 400);
-})->where('segment', '.*');
-
 Route::get('unauthorized', function () {
     return response()->json(['error' => 'Unauthorized.'], 401);
 })->name('unauthorized');
+
+Route::any('{segment}', function () {
+    return response()->json(['error' => 'Bad request.'], 400);
+})->where('segment', '.*');
